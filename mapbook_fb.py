6e0be9a3d8ab93d98aea196d39480f56844a4970
drunkrussiantankdriver <u64712@student.wat.edu.pl>
2025-04-09 18:58:59 +0200

    wip: remove user function

--- mapbook_fb.py
+++ mapbook_fb.py
@@ -1,6 +1,6 @@
 from not1 import add_user
 from utils.model import users
-from utils.controller import get_user_info, add_user
+from utils.controller import get_user_info, add_user, edit_user, users.pop
 
 
 def main():
@@ -12,13 +12,15 @@ def main():
         print('1 - pokaż co u znajomych')
         print('2 - dodaj nowego znajomego')
         print('3 - usuń znajomego')
+        print('4 - edytuj znajomego')
         print('==========================')
         choice = input('wybierz opcję MENU: ')
         if choice == '0': break
         if choice == '1': get_user_info(users)
         if choice == '2': add_user(users)
         if choice == '3': users.pop()
+        if choice == '4': edit_user(users)
 
 
-if __name__ == '__main__':
+
     main()
