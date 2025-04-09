f9a8900411398bdccb5034d9af67b3a00db872bd
drunkrussiantankdriver <u64712@student.wat.edu.pl>
2025-04-09 17:37:08 +0200

    chore: simple data structure improved

--- mapbook_fb.py
+++ mapbook_fb.py
@@ -3,7 +3,17 @@ from utils.controller import get_user_info
 
 
 def main():
-    get_user_info(users)
+    print(f'Witaj {users[0]['name']}')
+
+    while True:
+        print('===========MENU===========')
+        print('0 - zakończ program')
+        print('1 - pokaż co u znajomych')
+        print('==========================')
+        choice=input('wybierz opcję MENU: ')
+        if choice == '0': break
+        if choice == '1': get_user_info(users)
+
 
 if __name__ == '__main__':
     main()
--- utils/controller.py
+++ utils/controller.py
@@ -1,9 +1,3 @@
-users:list=[
-    {'name':'Filip','location':'Sieradz','posts:':2},
-    {'name':'Oliwier','location':'Zamość','posts:':3},
-    {'name':'Jakub','location':'Warszawa','posts:':500},
-    {'name':'Konrad','location':'Lublin','posts:':10},
-]
 def get_user_info(users_data:list)->None:
     for user in users_data:
-        print(f'witaj {user['name']}')
+        print(f'Twój znajomy {user['name']} z miejscowości {user['location']} opublikował {user['posts']} postów.')
