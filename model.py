f3c0ae9a20668ff4647876bbe14dfca6e606ada8
drunkrussiantankdriver <u64712@student.wat.edu.pl>
2025-06-26 14:48:19 +0200

    wip: update

--- model.py
+++
@@ -1,6 +0,0 @@
-gardeners: list[dict] = [
-    {'gardener_name': 'Tomasz Śledziona', 'gardener_company': 'MazurTrans', 'gardener_location': 'Warszawa'},
-    {'gardener_name': 'Tomasz Piotrowski', 'gardener_company': 'BogdPol', 'gardener_location': 'Sulejówek'},
-    {'gardener_name': 'Kamil Nowak', 'gardener_company': 'PiszPol', 'gardener_location': 'Kraków'},
-    {'gardener_name': 'Dominik Kowal', 'gardener_company': 'Transportex', 'gardener_location': 'Wieliczka'}
-]
